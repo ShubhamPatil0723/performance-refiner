6848176f1144f078cbe771e4774833944fd6c1fa
Shubham Patil <shubhampatil0723@gmail.com>
2022-12-29 11:10:11 +0530

    fetching functions created

---
+++ src/api/fetch.js
@@ -0,0 +1,16 @@
+const baseURL = "http://localhost:8080/auth";
+
+export const postData = (url, action, headers, data) => {
+    return fetch(`${baseURL}${url}`, {
+        method: action,
+        headers,
+        body: JSON.stringify(data),
+    });
+};
+
+export const getData = (url, action, headers) => {
+    return fetch(`${baseURL}${url}`, {
+        method: action,
+        headers,
+    });
+};
