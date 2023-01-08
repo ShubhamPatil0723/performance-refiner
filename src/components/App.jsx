afcdad4d543b6746b274f5bf5c97c9bf4ecb7899
Shubham Patil <shubhampatil0723@gmail.com>
2023-01-08 08:12:43 +0530

    add protected routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// //components
+// context
+import AuthProvider from "./context/AuthProvider";
+
+// components
 import NavBar from "./single-utility/Navbar";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import Contact from "./Pages/Contact";
 import Result from "./Pages/Result";
 import Footer from "./single-utility/Footer";
+import ChooseSub from "./Pages/ChooseSub";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 const App = () => {
     return (
@@ -15,6 +20,7 @@ const App = () => {
             <BrowserRouter>
                 <NavBar />
                 <Routes>
+                    {/* public */}
                     <Route path='/' exact='true' element={<HomePage />} />
                     <Route path='/login' exact='true' element={<LoginPage />} />
                     <Route
@@ -22,7 +28,16 @@ const App = () => {
                         exact='true'
                         element={<Contact />}
                     />
+                    {/* protected */}
                     <Route path='/result' exact='true' element={<Result />} />
+                    <Route
+                        path='/choose-sub'
+                        exact='true'
+                        element={<ChooseSub />}
+                    />
+
+                    {/* Missing */}
+                    <Route path='*' element={<NotFoundPage />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
