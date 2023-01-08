1ee79f75ab6f0036d68a02ea67ad7905ae47c632
Shubham Patil <shubhampatil0723@gmail.com>
2023-01-08 08:09:10 +0530

    Changed from recharts to react-chartjs

--- src/components/single-utility/AreaChartCreator.jsx
+++ src/components/single-utility/AreaChartCreator.jsx
@@ -1,47 +1,14 @@
 import React from "react";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-  ResponsiveContainer,
-} from "recharts";
 
 const areaData = [
-  { year: 2022, value: 30 },
-  { year: 2023, value: 40 },
-  { year: 2024, value: 50 },
-  { year: 2025, value: 35 },
+    { year: 2022, value: 30 },
+    { year: 2023, value: 40 },
+    { year: 2024, value: 50 },
+    { year: 2025, value: 35 },
 ];
 
 const AreaChartCreator = () => {
-  return (
-    <div className="AREA-CHART h-[50vh]">
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart data={areaData}>
-          <defs>
-            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="0%" stopColor="#245187" stopOpacity={0.4} />
-              <stop offset="75%" stopColor="#245187" stopOpacity={0.05} />
-            </linearGradient>
-          </defs>
-
-          <Area dataKey="value" stroke="#245187" fill="url(#color)" />
-          <XAxis dataKey="year" />
-          <YAxis
-            dataKey="value"
-            axisLine={false}
-            tickLine={false}
-            tickCount={5}
-          />
-          <Tooltip />
-          <CartesianGrid opacity={0.3} vertical={false} />
-        </AreaChart>
-      </ResponsiveContainer>
-    </div>
-  );
+    return <div className='AREA-CHART h-[50vh]'>Area chart</div>;
 };
 
 export default AreaChartCreator;
--- src/components/single-utility/PieChartCreator.jsx
+++ src/components/single-utility/PieChartCreator.jsx
@@ -1,62 +1,37 @@
-import { PieChart, Pie, ResponsiveContainer, Legend } from "recharts";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const pieData = [
-  { name: "Types of database", value: 60 },
-  { name: "OLAP", value: 20 },
-  { name: "Data Reduction", value: 10 },
-  { name: "Association rule mining", value: 20 },
-  { name: "Data warehousing", value: 10 },
-  { name: "KNN", value: 40 },
+    { name: "Types of database", value: 60 },
+    { name: "OLAP", value: 20 },
+    { name: "Data Reduction", value: 10 },
+    { name: "Association rule mining", value: 20 },
 ];
 
-const renderColorfulLegendText = (value, entry) => {
-  const { color } = "red";
-
-  return <span style={{ color }}>{value}</span>;
-};
-
-const renderedLegend = (props) => {
-  const { payload } = props;
-  return (
-    <ul className="text-white">
-      {payload.map((entry, index) => (
-        <li key={`item-${index}`}>{entry.value}</li>
-      ))}
-    </ul>
-  );
-};
-
 const PieChartCreator = () => {
-  return (
-    <div
-      className="PIE-CHART row-span-2
-                border border-[#00bd7e]"
-    >
-      <ResponsiveContainer width="80%" height="100%">
-        <PieChart>
-          <Legend
-            content={renderedLegend}
-            formatter={renderColorfulLegendText}
-            verticalAlign="middle"
-            align="right"
-            width="auto"
-            height="auto"
-            iconType="square"
-          />
-          <Pie
-            data={pieData}
-            dataKey="value"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
-            outerRadius={100}
-            innerRadius={80}
-            fill="#8884d8"
-          />
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
-  );
+    return (
+        <div className='PIE-CHART col-span-1 row-span-2'>
+            <Pie
+                data={{
+                    labels: [
+                        "NLP",
+                        "OLAP",
+                        "Data Reduction",
+                        "Association rule mining",
+                    ],
+                    datasets: [
+                        {
+                            label: "Topic-Wise Performance",
+                            data: pieData,
+                            backgroundColor: ["red", "blue", "green", "purple"],
+                        },
+                    ],
+                }}
+            />
+        </div>
+    );
 };
 
 export default PieChartCreator;
